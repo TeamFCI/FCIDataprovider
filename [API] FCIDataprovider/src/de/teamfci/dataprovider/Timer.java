5c9e89dab91716de916c8892a45742c77a578519
FilipZocktan <filipzocktan@gmail.com>
2016-03-18 21:16:55 +0100

    Added Timer cmd

--- [API] FCIDataprovider/src/de/teamfci/dataprovider/Timer.java
+++ [API] FCIDataprovider/src/de/teamfci/dataprovider/Timer.java
@@ -1,14 +1,31 @@
-package de.teamfci.dataprovider;
-
+	package de.teamfci.dataprovider;
+	
+	import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Timer implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		final String Timername = args[0];
+		final int length = Integer.parseInt(args[1]);
+			final Player p = (Player) sender;
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getServer().getPluginManager().getPlugin("FCIDataprovider"), new Runnable() {
+			int is = 0;
+			@Override
+			public void run() {
+				
+				if(is == length){
+					p.sendMessage("Der Timer " + Timername + " ist abgelaufen.");
+					return;
+				}
+				is++;
+				
+			}
+		}, 20, 20);
 		return false;
 	}
 
